d6ffe444660265e6d435a62d833a8da5e0c0f030
George King <george.w.king@gmail.com>
2017-01-03 11:12:46 -0500

    Optional: and(_, or:).

--- src/Quilt/Optional.swift
+++ src/Quilt/Optional.swift
@@ -11,6 +11,14 @@ extension Optional {
     }
   }
 
+  public func and<T>(_ body: (Wrapped) throws -> T, or alt: @autoclosure () throws -> T) rethrows -> T {
+    if let val = self {
+      return try body(val)
+    } else {
+      return try alt()
+    }
+  }
+
   public func or(_ alt: @autoclosure () throws -> Wrapped) rethrows -> Wrapped {
     if let val = self {
       return val
