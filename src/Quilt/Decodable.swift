c9bd33d6e7a1ad43d3c5b85943b0a4db36c39669
George King <george.w.king@gmail.com>
2017-07-15 12:18:58 -0400

    Decodable, Encodable extensions.

---
+++ src/Quilt/Decodable.swift
@@ -0,0 +1,12 @@
+// © 2017 George King. Permission to use this file is granted in license-quilt.txt.
+
+import Foundation
+
+
+extension Decodable {
+
+  init(jsonData: Data) throws {
+    let decoder = JSONDecoder()
+    self = try decoder.decode(Self.self, from: jsonData)
+  }
+}
