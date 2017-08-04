a302beaed0992e1183c890a1177950131c03fc66
George King <george.w.king@gmail.com>
2017-08-04 15:02:09 -0400

    NotificationCenter: observe, observeOnce.

--- src/Quilt/NotificationCenter.swift
+++ src/Quilt/NotificationCenter.swift
@@ -3,6 +3,27 @@
 import Foundation
 
 
-extension NotificationCenter {}
+public typealias Observer = NSObjectProtocol
+
 
 public let noteCenter: NotificationCenter = NotificationCenter.default
+
+extension NotificationCenter {
+
+  public func observe(_ object: Any, name: NSNotification.Name, queue: OperationQueue = OperationQueue.main,
+                      block: @escaping (Notification) -> Void) -> Observer {
+    return addObserver(forName: name, object: object, queue: queue, using: block)
+  }
+
+  public func observeOnce(_ object: Any, name: NSNotification.Name, queue: OperationQueue = OperationQueue.main,
+                          block: @escaping (Notification) -> Void) -> Observer {
+    let center = self
+    var observer: Observer!
+    observer = observe(object, name: name, queue: queue) {
+      (note) in
+      block(note)
+      center.removeObserver(observer)
+    }
+    return observer
+  }
+}
