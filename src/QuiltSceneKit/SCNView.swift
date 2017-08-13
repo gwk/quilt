8945176e54d5f550df486519b39d1f3dee798033
George King <george.w.king@gmail.com>
2017-08-13 09:15:23 -0400

    SCNView.hitTest.

--- src/QuiltSceneKit/SCNView.swift
+++ src/QuiltSceneKit/SCNView.swift
@@ -1,7 +1,20 @@
 // © 2016 George King. Permission to use this file is granted in license-quilt.txt.
 
 import SceneKit
+import QuiltBridge
 
 
 extension SCNView {
+
+  public func hitTest(event: CREvent, root: SCNNode, ignoreChildren: Bool,
+    sorted: Bool = true) -> [SCNHitTestResult] {
+
+    let p = self.convert(event.locationInWindow, from: nil)
+    return hitTest(p, options: [
+      .rootNode: root,
+      .ignoreChildNodes: ignoreChildren as NSNumber,
+      .sortResults: sorted as NSNumber
+    ])
+  }
+
 }
