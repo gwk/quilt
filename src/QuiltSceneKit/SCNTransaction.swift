ccb66cafc8acee75052e574ca817d933f760b806
George King <george.w.king@gmail.com>
2017-08-13 09:15:37 -0400

    SCNTransaction.animate.

---
+++ src/QuiltSceneKit/SCNTransaction.swift
@@ -0,0 +1,17 @@
+// © 2017 George King. Permission to use this file is granted in license-quilt.txt.
+
+import SceneKit
+import Quilt
+
+
+extension SCNTransaction {
+
+  class func animate(_ duration: F64 = 0.25, body: Action) {
+    begin()
+    animationDuration = duration
+    // TODO: set animationTimingFunction: CAMediaTimingFunction?
+    body()
+    commit()
+  }
+}
+
