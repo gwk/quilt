0910edd17f50f20c42b4de6e6cb0d6c92b0315a1
George King <george.w.king@gmail.com>
2017-09-03 11:35:56 -0700

    TextTreeStreamable.

---
+++ src/Quilt/TextTreeStreamable.swift
@@ -0,0 +1,32 @@
+// © 2017 George King. Permission to use this file is granted in license-quilt.txt.
+
+
+protocol TextTreeStreamable {
+  var textTreeHead: String { get }
+  var textTreeChildren: [Any] { get }
+}
+
+
+extension TextOutputStream {
+
+  mutating func writeTextTree(_ val: Any, depth: Int = 0) {
+    if depth > 0 { write(String(indent: depth)) }
+    if let val = val as? TextTreeStreamable {
+      write(val.textTreeHead)
+      let children = val.textTreeChildren
+      if children.isEmpty { return }
+      let childDepth = depth < 0 ? depth : depth + 1
+      write("(")
+      let childSep = depth < 0 ? " " : "\n" + String(indent: childDepth)
+      for child in val.textTreeChildren {
+        write(childSep)
+        writeTextTree(child, depth: childDepth)
+      }
+      write(")")
+    } else if let val = val as? TextOutputStreamable {
+      write(val)
+    } else {
+      write(String(describing: val))
+    }
+  }
+}
