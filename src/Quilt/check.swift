b709155c0c1900c17066269ca1122eced8fe2075
George King <george.w.king@gmail.com>
2017-10-29 15:00:10 -0400

    refactor check.swift.

--- src/Quilt/check.swift
+++ src/Quilt/check.swift
@@ -3,26 +3,22 @@
 import Darwin
 
 
-public func check<T>(_ condition: Bool, label: String = "error", _ messageItem: @autoclosure ()->T) {
-  if !condition {
-    if !label.isEmpty { errZ("\(label): ") }
-    errL(messageItem())
-    exit(1)
-  }
+public func fail<T>(label: String = "error", _ message: T) -> Never {
+  if !label.isEmpty { errZ("\(label): ") }
+  errL(message)
+  exit(1)
 }
 
-public func fail<T>(label: String = "error", _ messageItem: T) -> Never {
-  if !label.isEmpty { errZ("\(label): ") }
-  errL(messageItem)
-  exit(1)
+public func check<T>(_ condition: Bool, label: String = "error", _ messageItem: @autoclosure ()->T) {
+  if !condition {
+    fail(label: label, messageItem())
+  }
 }
 
 public func guarded<R>(label: String = "error", _ fn: () throws -> R) -> R {
   do {
     return try fn()
   } catch let e {
-    if !label.isEmpty { errZ("\(label): ") }
-    errL(e)
-    exit(1)
+    fail(label: label, e)
   }
 }
