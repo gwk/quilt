f21a974d842fd2ce29e1cbd5e4f5dfc6f2d15dc8
George King <george.w.king@gmail.com>
2017-10-29 15:01:59 -0400

    reinstate UInt8 ExpressibleByUnicodeScalarLiteral implementation.

--- src/Quilt/UInt8.swift
+++ src/Quilt/UInt8.swift
@@ -1,4 +1,10 @@
 // © 2017 George King. Permission to use this file is granted in license-quilt.txt.
 
 
-extension UInt8 {}
+extension UInt8: ExpressibleByUnicodeScalarLiteral {
+  public typealias UnicodeScalarLiteralType = Unicode.Scalar
+
+  public init(unicodeScalarLiteral value: Unicode.Scalar) {
+    self = UInt8(value.value)
+  }
+}
