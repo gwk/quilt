e63c6c7ef48900fb13032b4e5277668969152d47
George King <george.w.king@gmail.com>
2017-11-25 08:39:04 -0500

    Err implements CustomStringConvertible.

--- src/Quilt/Err.swift
+++ src/Quilt/Err.swift
@@ -1,10 +1,12 @@
 // © 2017 George King. Permission to use this file is granted in license-quilt.txt.
 
 
-public struct Err<T>: Error {
+public struct Err<T>: Error, CustomStringConvertible {
   let val: T
 
   init(_ val: T) {
     self.val = val
   }
+
+  public var description: String { return "Err(\(val))" }
 }
