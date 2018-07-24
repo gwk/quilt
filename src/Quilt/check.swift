c07d0b5fa19e295abd7e072d44af23d27310e895
George King <george.w.king@gmail.com>
2018-07-24 14:57:31 -0400

    assert functions.

--- src/Quilt/check.swift
+++ src/Quilt/check.swift
@@ -22,3 +22,45 @@ public func guarded<R>(label: String = "error", _ fn: () throws -> R) -> R {
     fail(label: label, e)
   }
 }
+
+public func assert( _ condition: Bool, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(condition, label: "", "\(file):\(line): assert failed.")
+  }
+}
+
+public func assertEq<T: Equatable>( _ l: T, _ r: T, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(l == r, label: "", "\(file):\(line): assertEq failed:\n  l: \(l)\n  r: \(r)")
+  }
+}
+
+public func assertNE<T: Equatable>( _ l: T, _ r: T, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(l != r, label: "", "\(file):\(line): assertNE failed:\n  l: \(l)\n  r: \(r)")
+  }
+}
+
+public func assertGE<T: Comparable>(_ l: T, _ r: T, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(l >= r, label: "", "\(file):\(line): assertGE failed:\n  l: \(l)\n  r: \(r)")
+  }
+}
+
+public func assertGT<T: Comparable>(_ l: T, _ r: T, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(l >  r, label: "", "\(file):\(line): assertGT failed:\n  l: \(l)\n  r: \(r)")
+  }
+}
+
+public func assertLE<T: Comparable>(_ l: T, _ r: T, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(l <= r, label: "", "\(file):\(line): assertLE failed:\n  l: \(l)\n  r: \(r)")
+  }
+}
+
+public func assertLT<T: Comparable>(_ l: T, _ r: T, file:StaticString=#file, line:UInt=#line) {
+  if _isDebugAssertConfiguration() {
+    check(l <  r, label: "", "\(file):\(line): assertLT failed:\n  l: \(l)\n  r: \(r)")
+  }
+}
