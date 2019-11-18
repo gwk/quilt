bf0e2eff5e51a1f946e4a8a34fa11cc1e8ddfe7a
George King <george.w.king@gmail.com>
2019-11-18 17:09:22 -0500

    fix Sequence test.

--- test/Quilt/Sequence.swift
+++ test/Quilt/Sequence.swift
@@ -6,15 +6,7 @@ import UTest
 
 func testSequence() {
 
-  //utest(seq: ["a" : ["a", "aa"], "b" : ["b", "bb"]],
-  //  ["", "a", "aa", "b", "bb"].group({$0.first}))
+  utest(exp: ["a" : ["a", "aa"], "b" : ["b", "bb"]],
+    (["", "a", "aa", "b", "bb"] as [String]).group({$0.first}))
 
-  // This mess is all due to SE 0143-conditional-conformances remaining unimplemented in swift 4.0.
-
-  let group_act = ["", "a", "aa", "b", "bb"].group({$0.first})
-  let group_exp: [Character:[String]] = ["a" : ["a", "aa"], "b" : ["b", "bb"]]
-  utest(seq: group_act.keys, group_exp.keys)
-  group_exp.forEach {
-    utest(exp: true, group_act[$0.key]! == $0.value)
-  }
 }
