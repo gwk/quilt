afa3d22351a85c48b9089a17c5ea3225d48c26fc
George King <george.w.king@gmail.com>
2019-12-03 08:13:53 -0500

    move Int extensions to BinaryInteger.

---
+++ src/Quilt/BinaryInteger.swift
@@ -0,0 +1,41 @@
+// © 2014 George King. Permission to use this file is granted in license-quilt.txt.
+
+
+public let digitChars = [Character]("0123456789abcdef")
+
+
+extension BinaryInteger {
+
+  public func repr(radix: Int = 10, pad: Character = " ", width: Int = 0) -> String {
+    let radix_ = Self(radix)
+    if self == 0 {
+      let count = Swift.max(0, width - 1)
+      return String(char: pad, count: count) + "0"
+    }
+    var a = [Character]()
+    let isNeg = (self < 0)
+    var i = self
+    var d: Self = 0
+    while i != 0 {
+      (i, d) = i.quotientAndRemainder(dividingBy: radix_)
+      a.append(digitChars[Int(d)])
+    }
+    if isNeg {
+      a.append("-")
+    }
+    var pad_len = width - (a.count + (isNeg ? 1 : 0))
+    while pad_len > 0 {
+      a.append(pad)
+      pad_len -= 1
+    }
+    return String(Array(a.reversed()))
+  }
+
+  public func decimal(width: Int = 0) -> String { return self.repr(radix: 10, width: width) }
+
+  public func hex(width: Int = 0) -> String { return self.repr(radix: 16, width: width) }
+
+  public func decimal0(width: Int = 0) -> String { return self.repr(radix: 10, pad: "0", width: width) }
+
+  public func hex0(width: Int = 0) -> String { return self.repr(radix: 16, pad: "0", width: width) }
+}
--- src/Quilt/Int.swift
+++ src/Quilt/Int.swift
@@ -1,39 +1,4 @@
 // © 2014 George King. Permission to use this file is granted in license-quilt.txt.
 
-
-public let digitChars = [Character]("0123456789abcdef")
-
 extension Int {
-
-  public func repr(radix: Int = 10, pad: Character = " ", width: Int = 0) -> String {
-    if self == 0 {
-      let count = Swift.max(0, width - 1)
-      return String(char: pad, count: count) + "0"
-    }
-    var a = [Character]()
-    let neg = (self < 0)
-    var i = neg ? -self : self
-    while i != 0 {
-      let d = i % radix
-      a.append(digitChars[d])
-      i /= radix
-    }
-    var pad_len = width - (a.count + (neg ? 1 : 0))
-    if neg {
-      a.append("-")
-    }
-    while pad_len > 0 {
-      a.append(pad)
-      pad_len -= 1
-    }
-    return String(Array(a.reversed()))
-  }
-
-  public func decimal(width: Int = 0) -> String { return self.repr(radix: 10, width: width) }
-
-  public func hex(width: Int = 0) -> String { return self.repr(radix: 16, width: width) }
-
-  public func decimal0(width: Int = 0) -> String { return self.repr(radix: 10, pad: "0", width: width) }
-
-  public func hex0(width: Int = 0) -> String { return self.repr(radix: 16, pad: "0", width: width) }
 }
