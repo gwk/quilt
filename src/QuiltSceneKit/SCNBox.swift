f61588bcca521df688ad958d262a008a94c09a5c
George King <george.w.king@gmail.com>
2020-09-04 13:08:30 -0400

    SCNBox.size.

---
+++ src/QuiltSceneKit/SCNBox.swift
@@ -0,0 +1,16 @@
+// © 2016 George King. Permission to use this file is granted in license-quilt.txt.
+
+import SceneKit
+
+
+public extension SCNBox {
+
+  var size: V3 {
+    get { return V3(width, height, length) }
+    set {
+      width = newValue.x
+      height = newValue.y
+      length = newValue.z
+    }
+  }
+}
