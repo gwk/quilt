bc1eb6da9ece53cbcf0cc3546faac997c3797d1d
George King <george.w.king@gmail.com>
2020-10-08 12:21:10 -0400

    DenseEnumArray.

---
+++ src/Quilt/DenseEnumArray.swift
@@ -0,0 +1,95 @@
+// © 2020 George King. Permission to use this file is granted in license-quilt.txt.
+
+
+public struct DenseEnumArray<Enum: DenseEnum, Element>: Collection, ExpressibleByArrayLiteral {
+
+  public typealias Index = Enum?
+  public typealias ArrayLiteralElement = Element
+
+
+  public struct Iterator : IteratorProtocol {
+    private var it: Array<Element>.Iterator
+
+    public init(_ array: DenseEnumArray) {
+      it = array.storage.makeIterator()
+    }
+
+    public mutating func next() -> Element? { it.next() }
+  }
+
+
+  private var storage: [Element]
+
+
+  public init() {
+    storage = []
+  }
+
+
+  public init<S: Sequence>(_ seq: S) where S.Element == Element {
+    storage = Array(seq)
+  }
+
+  public init(arrayLiteral elements: Element...) {
+    self.init(elements)
+  }
+
+  public init(pairs: (Enum, Element)...) {
+    self.init()
+    for (k, el) in pairs {
+      self[k] = el
+    }
+  }
+
+
+  public func makeIterator() -> Iterator { Iterator(self) }
+
+  public var isEmpty: Bool { count == 0 }
+
+  public var count: Int { storage.count }
+
+  public var startIndex: Index { Enum(rawValue: 0) }
+
+  public var endIndex: Index { storage.count < Enum.count ? Enum(rawValue: storage.count) : nil }
+
+  public var first: Element? { storage.first }
+  public var last: Element? { storage.last }
+
+
+  public subscript(index: Index) -> Element {
+    get {
+      guard let index = index else { fatalError("Index is nil") }
+      return storage[index.rawValue]
+    }
+    set {
+      guard let index = index else { fatalError("Index is nil") }
+      if index.rawValue == storage.count {
+        storage.append(newValue)
+      } else {
+        storage[index.rawValue] = newValue
+      }
+    }
+  }
+
+
+  public func index(after index: Index) -> Index {
+    guard let index = index else { return nil }
+    let nextIndex = index.rawValue + 1
+    if nextIndex >= Enum.count { return nil }
+    return Enum(rawValue: nextIndex)
+  }
+
+
+  public func index(where predicate: (Element) throws -> Bool) rethrows -> Index? {
+    if let i = try storage.firstIndex(where: predicate) {
+      return Enum(rawValue: i)
+    }
+    return nil
+  }
+
+
+  public mutating func append(_ el: Element) {
+    if count >= Enum.count { fatalError("DenseEnumArray is full; cannot append.") }
+    storage.append(el)
+  }
+}
