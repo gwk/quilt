d0c6997055d2f64a345b6ca861b94df38b9a7445
George King <george.w.king@gmail.com>
2020-10-14 23:40:07 -0400

    `apply` and `with` functions.

--- src/Quilt/func.swift
+++ src/Quilt/func.swift
@@ -7,3 +7,14 @@ public typealias Predicate = () -> Bool
 
 public let always: Predicate = { return true }
 public let never: Predicate = { return false }
+
+
+public func apply<T>(_ obj: T, body: (T)->()) -> T {
+  body(obj)
+  return obj
+}
+
+
+public func with<T>(_ obj: T, body: (T)->()) {
+  body(obj)
+}
