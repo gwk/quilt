7e7aa4df7e593de59be19b6a3f6cbf96662e2d28
George King <george.w.king@gmail.com>
2021-01-15 14:55:59 -0800

    LayerStyle.cornerMask.

--- src/QuiltUI/LayerStyle.swift
+++ src/QuiltUI/LayerStyle.swift
@@ -7,12 +7,20 @@ public class LayerStyle {
 
   public var color: CRColor
   public var cornerRadius: CGFloat
+  public var cornerMask: CACornerMask
   public var borderColor: CRColor
   public var borderWidth: CGFloat
 
-  public init(color: CRColor = .clear, cornerRadius: CGFloat = 0, borderColor: CRColor = .clear, borderWidth: CGFloat = 0) {
+  public init(
+    color: CRColor = .clear,
+    cornerRadius: CGFloat = 0,
+    cornerMask: CACornerMask = [.layerMinXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner],
+    borderColor: CRColor = .clear,
+    borderWidth: CGFloat = 0) {
+
     self.color = color
     self.cornerRadius = cornerRadius
+    self.cornerMask = cornerMask
     self.borderColor = borderColor
     self.borderWidth = borderWidth
   }
@@ -20,6 +28,7 @@ public class LayerStyle {
   public func apply(to layer: CALayer) {
     layer.color = color
     layer.cornerRadius = cornerRadius
+    layer.maskedCorners = cornerMask
     layer.borderColor = (borderColor == .clear ? nil : borderColor.cgColor)
     layer.borderWidth = borderWidth
   }
