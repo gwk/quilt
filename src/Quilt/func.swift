dd2a04464f3ba56cc3623635ff7d2bb0129f4987
George King <george.w.king@gmail.com>
2021-01-19 11:10:46 -0800

    `vary` function.

--- src/Quilt/func.swift
+++ src/Quilt/func.swift
@@ -1,5 +1,7 @@
 // © 2014 George King. Permission to use this file is granted in license-quilt.txt.
 
+import Foundation
+
 
 public typealias Action = () -> ()
 public typealias Predicate = () -> Bool
@@ -17,4 +19,11 @@ public func apply<T>(_ obj: T, body: (T)->()) -> T {
 
 public func with<T>(_ obj: T, body: (T)->()) {
   body(obj)
+}
+
+
+public func vary<T: NSCopying>(_ obj: T, body: (T) -> ()) -> T {
+  let c = obj.copy() as! T
+  body(c)
+  return c
 }
