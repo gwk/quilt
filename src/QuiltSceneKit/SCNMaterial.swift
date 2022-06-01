bd49efb67f02b92d56a3072b397e39150aea58be
George King <george@theorypractice.dev>
2022-06-01 09:40:53 -0400

    SCNMaterial.physMat.

--- src/QuiltSceneKit/SCNMaterial.swift
+++ src/QuiltSceneKit/SCNMaterial.swift
@@ -14,4 +14,15 @@ extension SCNMaterial {
 
   public static var flatWhite = SCNMaterial(flatColor: .white)
   public static var flatGray = SCNMaterial(flatColor: CRColor.gray)
+
+
+  public static func physMat(name: String? = nil, diffuse: Any? = nil, rough: Any? = nil, metal: Any?) -> SCNMaterial {
+    let mat = SCNMaterial()
+    mat.name = name
+    mat.lightingModel = .physicallyBased
+    mat.diffuse.contents = diffuse
+    mat.roughness.contents = rough
+    mat.metalness.contents = metal
+    return mat
+  }
 }
