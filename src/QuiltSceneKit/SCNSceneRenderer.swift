92ba47e64681d0cc7395bec47a2fd2b5293de82a
George King <george@theorypractice.dev>
2022-06-04 13:25:55 -0400

    SCNSceneRender.toggleShowsStatistics.

---
+++ src/QuiltSceneKit/SCNSceneRenderer.swift
@@ -0,0 +1,12 @@
+// © 2022 George King. Permission to use this file is granted in license-quilt.txt.
+
+import SceneKit
+import QuiltUI
+
+
+extension SCNSceneRenderer {
+
+  public func toggleShowsStatistics() {
+    showsStatistics = !showsStatistics
+  }
+}
